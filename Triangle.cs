f22281867560dc51f577ec572d0f994c0ba1513d
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-02-25 14:24:46 -0800

    refactor triangle tracker code

--- Triangle.cs
+++ Triangle.cs
@@ -6,18 +6,37 @@ public class Triangle
   public int Side1;
   public int Side2;
   public int Side3;
+
+  public Triangle(int Side1, int Side2, int Side3)
+  {
+      SideOne = Side1;
+      SideTwo = Side2;
+      SideThree = Side3;
+  }
 }
-public bool CheckSide(int Side1, int Side2, int Side3)
+public string CheckSide()
 {
-    
+    if(SideOne == SideTwo && SideOne == SideThree)
+    {
+        return "equilateral";
+    }
+    else if (SideOne == SideTwo || SideTwo == SideThree || SideThree == SideOne)
+    {
+        return "isosceles";
+    }
+    else
+    {
+        return "scalene";
+    }
 }
 
+public bool
+
+// secondary 
 public class Program
 {
   public static void Main()
   {
-      Triangle equilateral = new Triangle();
-      Triangle isosceles = new Triangle();
-      Triangle scalene = new Triangle();
+      
   }
 }
