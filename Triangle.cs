ffe8faecbf9e9830aa7123ef0b144dcbf3c866ba
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-02-25 13:18:16 -0800

    initial commit

---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class Triangle
+{
+  public int Side1;
+  public int Side2;
+  public int Side3;
+}
+public bool CheckSide(int Side1, int Side2, int Side3)
+{
+    
+}
+
+public class Program
+{
+  public static void Main()
+  {
+      Triangle equilateral = new Triangle();
+      Triangle isosceles = new Triangle();
+      Triangle scalene = new Triangle();
+  }
+}
